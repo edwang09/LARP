0fd3825156f0a63ec4b2a070c2934417142e331d
Nameedwang09 <edwang09@gmail.com>
2017-10-14 11:33:03 -0400

    shop backend

--- jianantesting/pages/shop/shop.js
+++ jianantesting/pages/shop/shop.js
@@ -1,6 +1,8 @@
 Page({
   data: {
     background: ['green', 'red', 'yellow'],
+    gamelist:[],
+    currentgame:{}
     
   },
   navigate: function (e) {
@@ -8,5 +10,45 @@ Page({
       url: '../shop/category/' + e.target.id,
     })
   },
+  introduction: function (e) {
+    let that = this
+    wx.request({
+      url: 'https://larpxiaozhushou.tk/api/game?id=' + e.target.id,
+      success: function (res) {
+        that.setData({
+          currentgame: res.data[0]
+        })
 
+      },
+      complete: function (){
+        var content = '游戏名称：' + that.data.currentgame.name + '； 游戏人数：' + that.data.currentgame.playernumber + '； 男性人数下限：' + that.data.currentgame.malenumber + '； 女性人数下限：' + that.data.currentgame.femalenumber + '； 游戏简介：' + that.data.currentgame.descripion
+        wx.showModal({
+          title: '详细介绍',
+          content: content,
+          confirmText:'创建',
+          success: function (res) {
+            if (res.confirm) {
+              wx.navigateTo({
+                url: '../create/create?gameid=' + that.data.currentgame.id
+              })
+            } else if (res.cancel) {
+              console.log('用户点击取消')
+            }
+          }
+        })
+      }
+    });
+    
+  },
+  onShow: function (e) {
+    let that = this
+    wx.request({
+      url: 'https://larpxiaozhushou.tk/api/game/',
+      success: function (res) {
+        that.setData({
+          gamelist: res.data
+        })
+      },
+    });
+  }
 })
