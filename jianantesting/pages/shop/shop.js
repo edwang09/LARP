22b7e8b94688e152ff30a1c63c67344bcdb2f329
Mikean16l <mikean16l@gmail.com>
2017-10-14 10:28:59 +0800

    Shop

---
+++ jianantesting/pages/shop/shop.js
@@ -0,0 +1,11 @@
+Page({
+  data: {
+    background: ['green', 'red', 'yellow'],
+  },
+  navigate: function (e) {
+    wx.navigateTo({
+      url: '../shop/category/' + e.target.id,
+    })
+  },
+
+})
